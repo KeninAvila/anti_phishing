fe4f20a836458899f5214b1eaa97e6518332e620
KeninAvila <149337855+KeninAvila@users.noreply.github.com>
2023-12-24 18:52:48 -0500

    Punto 2

--- src/main/java/com/mycompany/anti_phishing/Anti_phishing.java
+++ src/main/java/com/mycompany/anti_phishing/Anti_phishing.java
@@ -80,4 +80,45 @@ public class Anti_phishing{
             System.err.println("Archivo no encontrado: " + e.getMessage());
         }
     }
+    
+        private static int obtenerPuntosPorFrase(String frase) {
+        // Definir la asignación de puntos para cada frase
+        switch (frase) {
+            case "Verificar cuenta":
+            case "Confirmar contraseña":
+            case "Actualizar información":
+            case "PayPal":
+            case "Amazon":
+                return 3;
+            case "Urgente":
+            case "Acción requerida":
+            case "Contraseña expirada":
+            case "eBay":
+            case "Apple":
+            case "DHL":
+            case "FedEx":
+            case "Transferencia bancaria":
+            case "Impuestos":
+            case "Tarjeta de crédito":
+            case "Número de seguridad social":
+                return 2;
+            case "Cuenta suspendida":
+            case "Cuenta bloqueada":
+            case "Iniciar sesión":
+            case "Datos personales":
+            case "Información de pago":
+            case "Correo electrónico no solicitado":
+            case "Oferta limitada":
+            case "Ganaste":
+            case "Premio":
+            case "Lotería":
+            case "Herencia":
+            case "Donación":
+            case "Microsoft":
+            case "Google":
+                return 1;
+            default:
+                return 0; // Por defecto, no asigna puntos
+        }
+    }
 }
